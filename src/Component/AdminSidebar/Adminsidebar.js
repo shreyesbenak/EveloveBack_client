334f71e563263732d9ba0501066cd3f0ea16d43b
m-ksingh <mstict16@gmail.com>
2022-05-12 14:34:10 +0530

    responsive table , sidebar and signin done

--- src/Component/AdminSidebar/Adminsidebar.js
+++ src/Component/AdminSidebar/Adminsidebar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../Utils/Auth';
+import avtar from "../../image/default-avatar-profile-icon-vector-18942381.png"
 import "./Adminsidebar.css"
 
 function Adminsidebar() {
@@ -13,16 +14,17 @@ function Adminsidebar() {
    }
   return (
     <div className="sidebar1">
-    <div className="sidebar-top">
-        <i className="logo "></i>
-        <span className="brand">EMS</span>
-    </div>
+   <div class="profile">
+                <img src={avtar}alt="profile_picture"/>
+                <h3>Anamika Roy</h3>
+                <p>Designer</p>
+            </div>
     <div className="sidebar-center">
         <ul className="list">
            
             <li className="list-item">
               <Link to='/admin/viewdata' className='Link'>
-              <i className="list-item-icon fas fa-plus"></i>
+              <i className="list-item-icon fas fa-file"></i>
                 <span className="list-item-text">View Data</span>
 
               </Link>
--- src/Component/Sidebar/Sidebar.js
+++ src/Component/Sidebar/Sidebar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./Sidebar.css"
 import { Link } from 'react-router-dom'
+import avtar from "../../image/default-avatar-profile-icon-vector-18942381.png"
 // import { Redirect,Switch } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../Utils/Auth';
@@ -15,10 +16,12 @@ function Sidebar() {
    }
   return (
     <div className="sidebar">
-    <div className="sidebar-top">
-        <i className="logo "></i>
-        <span className="brand">EMS</span>
-    </div>
+    
+    <div class="profile">
+                <img src={avtar}alt="profile_picture"/>
+                <h3>Anamika Roy</h3>
+                <p>Designer</p>
+            </div>
     <div className="sidebar-center">
         <ul className="list">
            
